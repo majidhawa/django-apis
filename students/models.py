69b56ffadafc2050228c2d557e218963dc321941
majidhawa <majidhawaah@gmail.com>
2024-07-15 22:44:10 +0300

    first commit

---
+++ students/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models.manager import BaseManager
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    nationality = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    immediate_contact = models.CharField(max_length=100)
+    enrollment_date = models.DateField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    bio = models.TextField()
+    photo = models.ImageField(upload_to='photos/')
+    courses = models.ManyToManyField('course.Course', related_name='student')
+    classes = models.ManyToManyField('classes.Class', related_name='student')
+    
+    objects:BaseManager['Student']
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
---
+++ teachers/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from course.models import Course  
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=15)
+    nationality = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    email = models.EmailField(primary_key=True)
+    years_of_experience = models.IntegerField()
+    place_of_birth = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='photos/')
+    salary = models.IntegerField()
+    hire_date = models.DateField()
+    bio = models.TextField()
+    courses = models.ManyToManyField(Course, related_name='teachers')
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
